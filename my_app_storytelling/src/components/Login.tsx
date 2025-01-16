9b227a9e77b99ff29e9e55968906c6b5c8d68c24
Arielle5785 <arielle.benadi@gmail.com>
2025-01-16 14:09:53 +0200

    undifiend url

--- my_app_storytelling/src/components/Login.tsx
+++ my_app_storytelling/src/components/Login.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../auth/useAuth";
 
-const apiBaseUrl: string = import.meta.env.VITE_API_BASE_URL;
+const apiBaseUrl: string = import.meta.env.VITE_API_BASE_URL||"";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
